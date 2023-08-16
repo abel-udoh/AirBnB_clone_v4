9a18b9a7e6803deb3026fcd667a2597eda9be133
abel-udoh <abeludoh8@gmail.com>
2023-08-16 10:43:15 -0700

    web dynamic

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
-"""
-    Application for registering blueprint and starting flask
-"""
-
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -31,7 +29,6 @@ def not_found(error):
         description: a resource was not found
     """
     return make_response(jsonify({'error': "Not found"}), 404)
-
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
